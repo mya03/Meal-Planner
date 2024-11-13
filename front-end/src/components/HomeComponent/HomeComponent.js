fc227bbb60d01295daadfbb074eb3b3b4ece538e
Eris <khanhnguyengia2004@gmail.com>
2024-11-13 18:45:38 -0500

    implement base front-end for Home Page

--- front-end/src/components/HomeComponent/HomeComponent.js
+++ front-end/src/components/HomeComponent/HomeComponent.js
@@ -1,4 +1,5 @@
 import { BaseComponent } from '../BaseComponent/BaseComponent.js';
+import { EventHub } from '../../eventhub/EventHub.js'; 
 
 export class HomeComponent extends BaseComponent {
     #container = null; // Private variable to store the container element
@@ -16,6 +17,7 @@ export class HomeComponent extends BaseComponent {
         this.#createContainer();
         this.#createIntroContainer();
         this.#createRecommendedRecipeContainer();
+        this.#attachEventListeners();
        
         return this.#container;
     }
@@ -27,15 +29,25 @@ export class HomeComponent extends BaseComponent {
 
     #createIntroContainer(){
         const introContainer = document.createElement('div');
+        introContainer.classList.add('homeIntroContainer');
 
         const textContainer = document.createElement('div');
+        textContainer.classList.add('homeTextContainer');
         textContainer.innerHTML = `
-        <h2>Hey Jack</h2>
-        <p>Some text<p>
-        <button>Discover Receipts</button>
+        <h1>Hey Siri!</h1>
+        <p>
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Convallis est eget purus fringilla est cursus netus hac. 
+            Aenean nisl quis accumsan nisi posuere faucibus est taciti ex. 
+            Rhoncus lacus varius tortor tempor tincidunt luctus.
+        <p>
+        <button id="discoverBtn">Discover Receipts</button>
         `;
 
-        const imgContainer = document.createElement('div');
+        const imgContainer = document.createElement('img');
+        
+        imgContainer.src = 'https://www.pngall.com/wp-content/uploads/2/Meal-PNG-Download-Image.png';
+        //imgContainer.classList.add('homeImgContainer');
+        imgContainer.id = "homeImg"
 
         introContainer.appendChild(textContainer);
         introContainer.appendChild(imgContainer);
@@ -44,12 +56,17 @@ export class HomeComponent extends BaseComponent {
     }
 
     #createRecommendedRecipeContainer(){
+        const recTitle = document.createElement('h2');
+        recTitle.innerText = "Recommended Recipes";
+        recTitle.id = "recTitle";
         const recRecipeContainer = document.createElement('div');
+        recRecipeContainer.classList.add('homeRecipeContainer');
 
         for (let i=0; i < 3;i++){
             const recipeCard = document.createElement('div');
+            recipeCard.classList.add('homeRecipeCard');
             recipeCard.innerHTML = `
-                <img>
+                <div></div>
                 <h3>Name of the Recipe</h3>
                 <h4>Calories</h4>
             `;
@@ -57,8 +74,21 @@ export class HomeComponent extends BaseComponent {
             recRecipeContainer.appendChild(recipeCard);
         }
 
+        this.#container.appendChild(recTitle);
         this.#container.appendChild(recRecipeContainer);
 
+    }
+
+    #attachEventListeners(){
+        const hub = EventHub.getInstance();
+        const homeRecipeCards = this.#container.querySelectorAll('.homeRecipeCard');
+
+        // Loop through each element in the List of recipe cards and attach event
+        homeRecipeCards.forEach((homeRecipeCard) => {
+            homeRecipeCard.addEventListener('click', () => {
+                hub.publish('navigateToDetailedRecipe', null);
+            });
+        });
 
     }
     
