9c7e0811689f95eb9bb569d7a5f1878bbb97116d
Clary Nguyen <thuytiennguyen140103@gmail.com>
2024-11-16 17:56:25 -0500

    Integrate API call to spoonacular to get recipes and create functions to get a random recipe from the remote db

--- front-end/src/components/AppControllerComponent/AppControllerComponent.js
+++ front-end/src/components/AppControllerComponent/AppControllerComponent.js
@@ -1,3 +1,17 @@
 export class AppControllerComponent{
-    
+    #container = null;
+
+    constructor() {
+        this.#createContainer();
+    }
+
+    render() {
+        return this.#container;
+    }
+
+    // Creates the main container element
+    #createContainer() {
+        this.#container = document.createElement('div');
+        this.#container.classList.add('app-controller');
+    }
 }
